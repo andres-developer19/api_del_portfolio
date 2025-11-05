0bb43f92747c04b6e8d67e36f26278dc61915c06
andres <andres.developer19@gmail.com>
2025-11-05 13:17:49 +0100

    backup 6

--- experience/urls.py
+++ experience/urls.py
@@ -1,7 +1,11 @@
 from rest_framework import routers
 from .views import ExperienceViewSet
+from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register(r'experiences', ExperienceViewSet, basename='experience')
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
